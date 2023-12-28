e4b5b3c01377b648f456fdffeb0b385e041ca505
Jeremy A Gray <jeremy.a.gray@gmail.com>
2023-12-28 09:49:36 -0600

    release: prepare to tag 0.1.1
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "sigcalc"
 copyright = "2023, Jeremy A Gray"
 author = "Jeremy A Gray"
-release = "0.1.0"
+release = "0.1.1"
 
 extensions = [
     "sphinx.ext.autodoc",
