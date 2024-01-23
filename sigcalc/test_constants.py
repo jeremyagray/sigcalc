e4337396136bb17557ef8b8e7cc71ef338c7cbbe
Jeremy A Gray <jeremy.a.gray@gmail.com>
2024-01-22 21:28:00 -0600

    feat(sigcalc): compute cos and acos
    Implement cosine and inverse cosine, with tests.  Fix tests for sine
    and inverse sine.
    
    Github-closes: closes jeremyagray/sigcalc#8
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- sigcalc/test_constants.py
+++ sigcalc/test_constants.py
@@ -10,10 +10,9 @@
 #
 # ******************************************************************************
 
-"""Quantity class tests."""
+"""Constant tests."""
 
 from decimal import Decimal
-from decimal import getcontext
 
 import sigcalc
 
@@ -21,12 +20,12 @@ import sigcalc
 def test_properties_of_e():
     """Should have the correct properties."""
     assert sigcalc.e.value == Decimal("2.7182818284590452353602874713526624977572")
-    assert sigcalc.e.figures == getcontext().prec
+    assert sigcalc.e.figures == Decimal("41")
     assert sigcalc.e.constant is True
 
 
 def test_properties_of_pi():
     """Should have the correct properties."""
     assert sigcalc.pi.value == Decimal("3.1415926535897932384626433832795028841971")
-    assert sigcalc.pi.figures == getcontext().prec
+    assert sigcalc.pi.figures == Decimal("41")
     assert sigcalc.pi.constant is True
